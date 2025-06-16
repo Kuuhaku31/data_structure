f3f856a8c89f0325d3241e514a0388f505f6266f
Kuuhaku_kazari <SP_YYY@outlook.com>
2025-06-16 09:11:17 +0800

    优化哈希表相关函数的注释，增强代码可读性

--- src/linkhash.cpp
+++ src/linkhash.cpp
@@ -19,10 +19,13 @@ Hash(KeyType K, int m)
 void
 InitHash(HashTable& H, int size)
 {
+    // 确保哈希表大小大于0
     H.size  = size;
     H.count = 0;
     H.m     = size;
     H.rcd   = new ElemNode*[size];
+
+    // 初始化指针数组，每个元素指向 nullptr
     for(int i = 0; i < size; ++i)
     {
         H.rcd[i] = nullptr;
@@ -33,16 +36,20 @@ InitHash(HashTable& H, int size)
 void
 DestroyHash(HashTable& H)
 {
+    // 遍历每个链表，释放节点内存
     for(int i = 0; i < H.size; ++i)
     {
-        ElemNode* p = H.rcd[i];
+        ElemNode* tmp = nullptr;
+        ElemNode* p   = H.rcd[i];
         while(p)
         {
-            ElemNode* tmp = p;
-            p             = p->next_node;
+            tmp = p;
+            p   = p->next_node;
             delete tmp;
         }
     }
+
+    // 释放指针数组内存
     delete[] H.rcd;
     H.rcd  = nullptr;
     H.size = H.count = H.m = 0;
@@ -52,12 +59,14 @@ DestroyHash(HashTable& H)
 ElemNode*
 SearchHash(const HashTable& H, KeyType key)
 {
-    int       index = Hash(key, H.m);
-    ElemNode* p     = H.rcd[index];
+    // 计算哈希值
+    int index = Hash(key, H.m);
+
+    // 遍历对应链表查找
+    ElemNode* p = H.rcd[index];
     while(p)
     {
-        if(p->elem.key == key)
-            return p;
+        if(p->elem.key == key) return p;
         p = p->next_node;
     }
     return nullptr;
@@ -67,30 +76,48 @@ SearchHash(const HashTable& H, KeyType key)
 void
 InsertHash(HashTable& H, ElemType e)
 {
-    int       index    = Hash(e.key, H.m);
-    ElemNode* newNode  = new ElemNode{ e, nullptr };
-    newNode->next_node = H.rcd[index];
-    H.rcd[index]       = newNode;
-    ++H.count;
+    int index = Hash(e.key, H.m);
+
+    // 检查是否已存在相同的键
+    bool      found = false;
+    ElemNode* p     = H.rcd[index];
+    while(p)
+    {
+        if(p->elem.key == e.key && p->elem.data == e.data)
+        {
+            found = true; // 已存在相同键
+            break;
+        }
+        p = p->next_node;
+    }
+
+    // 如果不存在相同键，则插入新节点
+    if(!found)
+    {
+        ElemNode* newNode  = new ElemNode{ e, nullptr };
+        newNode->next_node = H.rcd[index];
+        H.rcd[index]       = newNode;
+        H.count++;
+    }
 }
 
 // 删除
 void
 DeleteHash(HashTable& H, ElemType e)
 {
-    int       index = Hash(e.key, H.m);
-    ElemNode* p     = H.rcd[index];
-    ElemNode* prev  = nullptr;
+    int index = Hash(e.key, H.m);
+
+    ElemNode* prev = nullptr;      // 前一个节点指针
+    ElemNode* p    = H.rcd[index]; // 当前节点指针
     while(p)
     {
         if(p->elem.key == e.key)
         {
-            if(prev)
-                prev->next_node = p->next_node;
-            else
-                H.rcd[index] = p->next_node;
+            if(prev) prev->next_node = p->next_node; // 如果不是头节点，前一个节点指向下一个节点
+            else H.rcd[index] = p->next_node;        // 如果是头节点，直接更新头指针
+
             delete p;
-            --H.count;
+            H.count--;
             return;
         }
         prev = p;
